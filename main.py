e2cc3668b17213642f649a58c41a85ea22590847
Rakesh Yadav <Rakesh.Yadav35@T-mobile.com>
2026-06-16 15:03:04 -0400

    added langchain tavily

--- main.py
+++ main.py
@@ -4,26 +4,33 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
 
-tavily = TavilyClient()
+# commenting for openai tavily -STEP 1
+#from tavily import TavilyClient
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
+#tavily = TavilyClient()
+
+#@tool
+#def search(query: str) -> str:
+   # """
+   # Tool that searches over internet
+   # Args:
+   #     query: The query to search for
+   # Returns:
+   #     The search result
+   # """
+ #   print(f"Searching for {query}")
+  #  return tavily.search(query=query)
+
+  # STEP- 2 test
+from langchain_tavily import TavilySearch
 
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
-agent = create_agent(model=llm,tools=tools)
+#tools = [search]
+#agent = create_agent(model=llm,tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
 
 
 def main():
